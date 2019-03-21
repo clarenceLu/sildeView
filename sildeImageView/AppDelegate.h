908b2b56cbff3bf7c7ba045de2c24ff6112b8d53
Clarence <ljwtomlu@gmail.com>
2019-03-21 09:56:45 +0800

    upload silde view

---
+++ sildeImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sildeImageView
+//
+//  Created by lujiawei on 2019/3/21.
+//  Copyright © 2019 clarence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sildeImageView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  sildeImageView
+//
+//  Created by lujiawei on 2019/3/21.
+//  Copyright © 2019 clarence. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
